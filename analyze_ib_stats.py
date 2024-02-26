da70516a17f4e97fec2989007c39d677d4fe4ece
William Seto <william.seto@jpl.nasa.gov>
2024-02-26 11:57:58 -0800

    found balance days

--- analyze_ib_stats.py
+++ analyze_ib_stats.py
@@ -4,8 +4,13 @@ import numpy as np
 import pdb
 from scipy import stats, signal
 import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
 
 stats_df = pd.read_csv('test_time_stats_full.csv')
+
+stats_df = stats_df.assign(DT='NAN')
+stats_df["DT"] = stats_df["Date"] + stats_df["Time"]
+stats_df['DT'] = pd.to_datetime(stats_df['DT'])
 
 trading_day_groups = stats_df.groupby(['trading_day'])
 
@@ -18,6 +23,15 @@ bal_tst = 0
 
 lo_ext = []
 hi_ext = []
+pchg = []
+other_lo_ext = []
+other_hi_ext = []
+up_pchg = []
+down_pchg = []
+ud_pchg = []
+up_tst = 0
+down_tst = 0
+ud_tst = 0
 for i in range(len(trading_day_groups)):
 
     td = i + td_offset
@@ -34,12 +48,56 @@ for i in range(len(trading_day_groups)):
     if not ib_stats:
         continue
 
-    curr_atr = stats_df.loc[curr_td_df.index[0], 'ATR']
+    curr_open = curr_td_df[curr_td_df['Time'] == " 09:30:00.0"]['Open']
+    curr_close = curr_td_df[curr_td_df['Time'] == " 15:59:00.0"]['Close']
 
-    rth_range = ib_stats['rth_hi'] - ib_stats['rth_lo']
+    if curr_open.empty or curr_close.empty:
+        continue
 
-    if rth_range < (curr_atr * 1):
+    curr_open = curr_open.values[0]
+    curr_close = curr_close.values[0]
+    curr_pchg = (curr_close - curr_open) / curr_open * 100.0
+
+    time_df = curr_td_df.set_index("DT")
+    rth_df = time_df.between_time('9:30', '15:59')
+
+    total_volume = rth_df['Volume'].sum()
+
+    ib_ext = 0.25
+    ib_hi_ext = (ib_stats['ib_range'] * ib_ext) + ib_stats['ib_hi']
+    ib_lo_ext = ib_stats['ib_lo'] - (ib_stats['ib_range'] * ib_ext)
+
+    ib_price_df = rth_df[(rth_df['Close'] > ib_lo_ext) & (rth_df['Close'] < ib_hi_ext)]
+
+    ib_volume = ib_price_df['Volume'].sum()
+
+    if (ib_volume / total_volume) > 0.9:
         bal_tst = bal_tst + 1
+        lo_ext.append(ib_stats['ib_lo_ext'])
+        hi_ext.append(ib_stats['ib_hi_ext'])
+    else:
+        if ib_stats['ib_lo_ext'] < 0.25 and ib_stats['ib_hi_ext'] > 0.25:
+            up_tst = up_tst + 1
+            up_pchg.append(curr_pchg)
+
+        elif ib_stats['ib_hi_ext'] < 0.25 and ib_stats['ib_lo_ext'] > 0.25:
+            down_tst = down_tst + 1
+            down_pchg.append(curr_pchg)
+
+        elif ib_stats['ib_hi_ext'] > 0.25 and ib_stats['ib_lo_ext'] > 0.25:
+            ud_tst = ud_tst + 1
+            ud_pchg.append(curr_pchg)
+
+
+    # curr_atr = stats_df.loc[curr_td_df.index[0], 'ATR']
+
+    # rth_range = ib_stats['rth_hi'] - ib_stats['rth_lo']
+
+    # if rth_range < (curr_atr * 0.5):
+    #     bal_tst = bal_tst + 1
+    #     lo_ext.append(ib_stats['ib_lo_ext'])
+    #     hi_ext.append(ib_stats['ib_hi_ext'])
+    #     pchg.append(curr_pchg)
 
     # curr_close = curr_td_df[curr_td_df['Time'] == " 15:59:00.0"]['Close']
     # if curr_close.empty:
@@ -47,18 +105,26 @@ for i in range(len(trading_day_groups)):
     # curr_close = curr_close.values[0]
 
     # if ib_stats['ib_hi_ext'] < 0.5 and ib_stats['ib_lo_ext'] < 0.5:
-    #     balance_days.append(i)
+    #     bal_tst = bal_tst + 1
+
 
     # if curr_close > ib_stats['ib_lo'] and curr_close < ib_stats['ib_hi']:
     #     bal_tst = bal_tst + 1
 
-        # print(i, ib_stats)
 
-    # lo_ext.append(ib_stats['ib_lo_ext'])
-    # hi_ext.append(ib_stats['ib_hi_ext'])
+print(bal_tst, up_tst, down_tst, ud_tst)
+
+# plt.scatter(lo_ext, hi_ext)
+# plt.scatter(other_lo_ext, other_hi_ext)
+# h = plt.hist2d(other_lo_ext, other_hi_ext, norm=LogNorm())
+# plt.colorbar(h[3])
+# plt.hist(pchg, bins=100)
+
+# plt.hist(up_pchg, bins=100)
+# plt.title('up')
+
+# plt.figure()
+plt.hist(ud_pchg, bins=100)
 
 
-
-# print(len(full_balance_days), len(expansion_days), real_num_days)
-
-print(bal_tst)
+plt.show()
